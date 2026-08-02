6462f8099f3562018cf2be5844eb625747dc777d
yaratul2005 <ratul41g@gmail.com>
2026-08-02 13:03:30 +0600

    fix(ui): add SystemSettingsView.xaml.cs code-behind file to properly execute InitializeComponent

---
+++ src/YAERP.UI/Views/SystemSettingsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace YAERP.UI.Views;
+
+public partial class SystemSettingsView : UserControl
+{
+    public SystemSettingsView()
+    {
+        InitializeComponent();
+    }
+}
